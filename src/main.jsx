e0fb53ebfe3592ec6d21b6cecce9491f6664da41
ii-shimul <islamshimul27@gmail.com>
2025-04-05 20:28:43 +0600

    added firebase and some hooks

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router}></RouterProvider>
-		</QueryClientProvider>
+		<AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router}></RouterProvider>
+			</QueryClientProvider>
+		</AuthProvider>
 	</StrictMode>
 );
