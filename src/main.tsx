a8c7870f7fb61c6599d291032881045de6b05efb
bax <islamshimul27@gmail.com>
2025-09-24 21:50:49 +0600

    converted main.jsx and router.jsx to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/router.jsx";
-import AuthProvider from "./providers/AuthProvider.jsx";
+import router from "./routes/router";
+import AuthProvider from "./providers/AuthProvider";
 import "@ant-design/v5-patch-for-react-19";
 const queryClient = new QueryClient();
 import { unstableSetRender } from "antd";
 
 unstableSetRender((node, container) => {
-	container._reactRoot ||= createRoot(container);
-	const root = container._reactRoot;
-	root.render(node);
-	return async () => {
-		await new Promise((resolve) => setTimeout(resolve, 0));
-		root.unmount();
-	};
+  const c = container as any;
+  c._reactRoot ||= createRoot(container as HTMLElement);
+  const root = c._reactRoot as any;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
 });
 
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<AuthProvider>
 			<QueryClientProvider client={queryClient}>
